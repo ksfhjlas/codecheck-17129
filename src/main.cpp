cb13f86d920851576b400489f25f4c3692e7d86b
ksfhjlas <science_yaba32204@softbank.ne.jp>
2017-10-05 02:15:11 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
   for (int i = 1; i < argc; i++) {
-    if(argv[i].size()==0){
+    if(i==2){
       printf ("Hello!\n");
     }else{
       printf ("Hello %s!\n", argv[i]);
