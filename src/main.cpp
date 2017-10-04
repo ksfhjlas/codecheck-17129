cc88f14b21c33a8f9fe13d773be45a6204bd6bae
ksfhjlas <science_yaba32204@softbank.ne.jp>
2017-10-05 02:05:45 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char *argv[])
 {
   // start from 1 to ignore script name; argv[0] will be a name of processing file.
   for (int i = 1; i < argc; i++) {
-    int len=argv[i].size();
+    char moji=argv[i];
+    int len=moji.size();
     printf("%d\n", len);
     if(argv[i]==""){
       printf ("Hello!\n");
